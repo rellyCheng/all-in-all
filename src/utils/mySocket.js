39e77509a01f24343c3c295b10d45621d2fb3cda
rellyCheng <707471634@qq.com>
2019-02-24 22:28:53 +0800

    socket配置

--- src/utils/mySocket.js
+++ src/utils/mySocket.js
@@ -4,9 +4,6 @@ import token from '@/utils/token';
 // export default socket;
 
 export function socket() {
-  let SOCKET_SERVER = 'http://118.24.218.25:9090';
-  if(process.env.API_ENV=='dev'){
-    SOCKET_SERVER = 'http://192.168.1.160:9090';
-  }    
-  return socket = io.connect(`${SOCKET_SERVER}?token=${token.get()}`);
+  
+  return socket = io.connect(SERVER_IP.SOCKET+`?token=${token.get()}`);
 }
